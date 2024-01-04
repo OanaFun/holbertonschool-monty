5d713ca989765b116cc3c6a9aacb36f158db524b
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-01-04 20:01:51 +0100

    modif check_opcodes

--- check_opcodes.c
+++ check_opcodes.c
@@ -12,12 +12,12 @@ void (*check_opcodes(void))(stack_t **, unsigned int)
     int len_opcode;
 
     instruction_t opcodes[] = {
-        {"push", push},
+       // {"push", push},
         {"pall", pall},
         {"pint", pint},
         {"pop", pop},
-        {"swap", swap},
-        {"nop", nop},
+       // {"swap", swap},
+       // {"nop", nop},
     };
 
     while (opcodes[i].opcode)
