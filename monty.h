37cda5f929f1b14be02c76b8b8f4474efc4ab873
OanaFun <7491@holbertonstudents.com>
2024-01-05 01:52:48 -0800

    Add codes that implement push and swap opcodes

--- monty.h
+++ monty.h
@@ -42,5 +42,9 @@ typedef struct instruction_s
 
 void (*check_opcodes(void))(stack_t **, unsigned int);
 void pop(stack_t **stack, unsigned int line_cnt);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
 
 #endif
